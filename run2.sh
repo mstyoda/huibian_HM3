0380a04cb0f90abec287628ade6f43734211883f
骆轩源 <luoxy15@mails.tsinghua.edu.cn>
2017-09-03 11:12:50 +0800

    fun

--- run2.sh
+++ run2.sh
@@ -1,5 +1,5 @@
 git pull
-as -gstabs -o alloc.o alloc.s --32
+as -gstabs -o newalloc.o newalloc.s --32
 as -gstabs -o test.o test.s --32
 as -gstabs -o base.o base.s --32
 ld -dynamic-linker /lib/ld-linux.so.2 -lc -m elf_i386 -o test2 test.o newalloc.o base.o
