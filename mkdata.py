030b561108fde9f3551544c40b524b29f54f11f1
骆轩源 <luoxy15@mails.tsinghua.edu.cn>
2017-09-03 21:49:09 +0800

    fun

--- mkdata.py
+++ mkdata.py
@@ -9,7 +9,7 @@ msg:
 .globl _start
 _start:
 	movl %esp,%ebp
-	subl $50,%esp
+	subl $5000,%esp
 
 	call allocate_init
 
@@ -24,15 +24,15 @@ out.write('''
 
 ''')
 
-n = 500000
+n = 100
 a = []
 cnt = 0
 for i in range(0,n):
 	per = random.randint(1,100)
-	if (per <= 100) or (len(a) == 0):
-		#cnt += 1
-		cnt = 1
-		size = random.randint(1,1)
+	if (per <= 60) or (len(a) == 0):
+		cnt += 1
+		#cnt = 1
+		size = random.randint(1,100)
 		a.append(cnt)
 		out.write('''
 	#begin allocate id = %d
